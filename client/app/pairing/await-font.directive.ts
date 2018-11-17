afd72885b1c0e816b32435b5d409dd680c432c41
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-11-17 23:33:16 +0100

    Reduce font loading timeout and use system font fallback

--- client/app/pairing/await-font.directive.ts
+++ client/app/pairing/await-font.directive.ts
@@ -1,10 +1,4 @@
-import {
-  Directive,
-  ElementRef,
-  HostBinding,
-  Input,
-  OnInit,
-} from "@angular/core"
+import { Directive, ElementRef, Input, OnInit } from "@angular/core"
 
 import * as FontFaceObserver from "fontfaceobserver"
 
@@ -12,27 +6,37 @@ export interface AwaitFontOptions {
   family: string
   weight?: number
   style?: string
+  timeout?: number
 }
 
 @Directive({
   selector: "[appAwaitFont]",
 })
 export class AwaitFontDirective implements OnInit {
-
   @Input("appAwaitFont") options: AwaitFontOptions
 
-  @HostBinding("style.opacity")
-  opacity = 0
+  private readonly el: ElementRef
 
   constructor(el: ElementRef) {
+    this.el = el
     el.nativeElement.style.opacity = "0"
     el.nativeElement.style.transition = "0.5s ease-out opacity"
   }
 
-  ngOnInit() {
-    const { family, ...options } = this.options
-    const font = new FontFaceObserver(family, options)
-    font.load().then(() => this.opacity = 1)
+  private showContent = () => {
+    this.el.nativeElement.style.opacity = 1
   }
 
+  ngOnInit() {
+    const { family, ...options } = this.options
+    new FontFaceObserver(family, options)
+      .load(null, options.timeout || 200)
+      .then(() => {
+        this.showContent()
+      })
+      .catch(error => {
+        console.error(error)
+        this.showContent()
+      })
+  }
 }
